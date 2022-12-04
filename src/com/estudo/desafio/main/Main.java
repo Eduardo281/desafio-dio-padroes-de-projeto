b8a369e553bdbd6ffd61e691528a62b56d5984c2
Eduardo <edteixeira28@hotmail.com>
2022-12-03 21:21:26 -0300

    Primeiro commit de Main.java

---
+++ src/com/estudo/desafio/main/Main.java
@@ -0,0 +1,36 @@
+package com.estudo.desafio.main;
+
+import com.estudo.desafio.exemplos.PrototypeExemplo;
+import com.estudo.desafio.exemplos.SingletonExemplo;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("*****");
+        System.out.println("Sobre Singleton:");
+        SingletonExemplo objeto1 = SingletonExemplo.getInstancia();
+        SingletonExemplo objeto2 = SingletonExemplo.getInstancia();
+        if(objeto1 == objeto2){
+            System.out.println("\tOs dois objetos obtidos via Singleton são o mesmo!");
+        }
+        else{
+            System.out.println("\tOs objetos obtidos via Singleton são diferentes!");
+        }
+        System.out.println();
+
+        System.out.println("Sobre Prototype:");
+        PrototypeExemplo pessoa1 = new PrototypeExemplo("José da Silva", 49, "36548-27");
+        PrototypeExemplo pessoa2 = pessoa1.clone();
+        System.out.println("\tPessoa 1 é:");
+        System.out.println("\t\t"+pessoa1);
+        System.out.println("\tPessoa 2 é:");
+        System.out.println("\t\t"+pessoa2);
+        if(pessoa1 == pessoa2){
+            System.out.println("\tOs dois objetos obtidos via Prototype são o mesmo!");
+        }
+        else{
+            System.out.println("\tOs objetos obtidos via Prototype são diferentes!");
+        }
+        System.out.println();
+        System.out.println("*****");
+    }
+}

bcac1fc603715653900462ca8f884d9da9c1fffd
Eduardo <edteixeira28@hotmail.com>
2022-12-03 21:46:55 -0300

    Classes auxiliares para o exemplo de Proxy

---
+++ src/com/estudo/desafio/exemplos/Cofre.java
@@ -0,0 +1,37 @@
+package com.estudo.desafio.exemplos;
+
+public class Cofre implements ICofre{
+    private boolean cofreEstaAberto = false;
+
+    @Override
+    public void abrirCofre() {
+        System.out.println("O cofre foi aberto!");
+        cofreEstaAberto = true;
+    }
+
+    @Override
+    public void fecharCofre() {
+        System.out.println("O cofre foi fechado!");
+        cofreEstaAberto = false;
+    }
+
+    @Override
+    public void verEstado(){
+        if(cofreEstaAberto){
+            System.out.println("O cofre está aberto!");
+        }
+        else{
+            System.out.println("O cofre está fechado!");
+        }
+    }
+
+    @Override
+    public void verConteudo(){
+        if(cofreEstaAberto){
+            System.out.println("O cofre contém alguma quantia em dinheiro.");
+        }
+        else{
+            System.out.println("Impossível ver conteúdo! O cofre está fechado!");
+        }
+    }
+}
---
+++ src/com/estudo/desafio/exemplos/ICofre.java
@@ -0,0 +1,8 @@
+package com.estudo.desafio.exemplos;
+
+public interface ICofre {
+    public void abrirCofre();
+    public void fecharCofre();
+    public void verEstado();
+    public void verConteudo();
+}

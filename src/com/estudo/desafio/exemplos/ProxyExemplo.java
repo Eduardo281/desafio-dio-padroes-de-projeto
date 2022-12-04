6ad9d40ca078d4406e85ffe427372bfa553e3563
Eduardo <edteixeira28@hotmail.com>
2022-12-03 21:47:04 -0300

    Exemplo de Proxy

---
+++ src/com/estudo/desafio/exemplos/ProxyExemplo.java
@@ -0,0 +1,34 @@
+package com.estudo.desafio.exemplos;
+
+public class ProxyExemplo {
+    protected ICofre cofre;
+
+    public ProxyExemplo(ICofre cofre) {
+        this.cofre = cofre;
+    }
+
+    public void abrirCofre(String senha){
+        if(autenticarSenha(senha)){
+            cofre.abrirCofre();
+        }
+        else{
+            System.out.println("Senha inválida! Não foi possível abrir o cofre!");
+        }
+    }
+
+    public boolean autenticarSenha(String senha){
+        return senha == "senhasenha123";
+    }
+
+    public void fecharCofre(){
+        cofre.fecharCofre();
+    }
+
+    public void verConteudo(){
+        cofre.verConteudo();
+    }
+
+    public void verEstado(){
+        cofre.verEstado();
+    }
+}
